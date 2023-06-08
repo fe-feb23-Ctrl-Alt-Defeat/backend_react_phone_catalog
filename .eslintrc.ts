11981505bbb87b26a6572fc3f5a812d482f21239
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-06-08 14:50:57 +0300

    fixed

---
+++ .eslintrc.ts
@@ -0,0 +1,13 @@
+export const rules = {
+  extends: '@mate-academy/eslint-config',
+  env: {
+    mocha: true,
+  },
+  globals: {
+    expect: 'readonly',
+  },
+  rules: {
+    'no-proto': 0
+  },
+  plugins: ['jest']
+};
