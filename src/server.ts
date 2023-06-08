b066ccd0dbe154db33e034fe64733d211774c7b7
Kyryl <kika_domination@yahoo.com>
2023-06-08 21:27:18 +0300

    add port

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import cors from 'cors';
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
