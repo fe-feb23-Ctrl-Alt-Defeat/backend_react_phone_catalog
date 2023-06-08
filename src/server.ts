80aa32dc124180e89657a50a74c4b8f07a42f267
Kyryl <kika_domination@yahoo.com>
2023-06-08 17:41:19 +0300

    remove port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import expres from 'express';
 import cors from 'cors';
 
 const app = expres();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(expres.json());
@@ -10,6 +9,6 @@ app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
 });
