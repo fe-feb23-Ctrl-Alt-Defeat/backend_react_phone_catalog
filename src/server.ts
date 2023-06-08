53359d4a8e136a14f8350c976b64fea4b95e7287
Kyryl <kika_domination@yahoo.com>
2023-06-08 17:00:18 +0300

    add sequalize and run lint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import expres from 'express';
 import cors from 'cors';
 
 const app = expres();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(expres.json());
@@ -9,6 +10,6 @@ app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
