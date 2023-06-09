2e6173b0e6100c68aa2ed0c028870ee361623bf9
Oleh Kolodii <bigknife4@gmail.com>
2023-06-09 20:37:35 +0300

    implemented static image server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { productsRouter } from "./routes/products";
 import { dbInit } from "./utils/dbInit";
+import path from 'path';
 
 const app = express();
 
@@ -12,6 +13,7 @@ dbInit();
 app.use(cors());
 app.use(express.json());
 app.use("/products", productsRouter);
+app.use(express.static(path.resolve('public')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
