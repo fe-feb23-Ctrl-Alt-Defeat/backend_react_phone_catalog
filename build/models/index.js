1f3cbbf6a03b5ab1423026e500f64fd73cc1714d
Oleh Kolodii <bigknife4@gmail.com>
2023-06-09 15:10:16 +0300

    build removed

--- build/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.models = void 0;
-const Product_1 = require("./Product");
-exports.models = [
-    Product_1.Product,
-];
